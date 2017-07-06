c8b26745a89a0f1e93b54853c0cb975c59f123df
yangjiaoshan <xuyunfei235@163.com>
2017-07-06 16:52:36 +0800

    version 0.01

--- Excel_Report.py
+++ Excel_Report.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #-*- coding:utf8 -*-
 import os, sys
 import time
@@ -24,7 +25,7 @@ def load_log():
     IntactLogPath = os.path.join(DataDir, IntactLog)
     SystemLogPath = os.path.join(DataDir, SystemLog)
     if not os.path.exists(SystemLogPath) :
-        print "No such file: " + SystemLogPath
+        print "NO such file: " + SystemLogPath
      
     fsystem = open(SystemLogPath,'rb')
     for record in fsystem :
@@ -89,45 +90,46 @@ def Create_Header(SystemSheet, IntactSheet):
                                          'valign':'vcenter',
                                          'fg_color':'8DB4E3'
                                          })
-#璁剧疆绯荤粺杩愯鐘舵�佺殑鏍囬
+#设置系统运行状态的标题
     SystemSheet.set_column('A:A',10)
     SystemSheet.set_column('B:J',14)
-    SystemSheet.merge_range('A1:A2', u'鐪佷唤', header_format)
-    SystemSheet.merge_range('B1:B2', u'鏈嶅姟鍣↖P', header_format)
-    SystemSheet.merge_range('C1:D1', u'鏈嶅姟鍣ㄧ姸鎬佺洃鎺�', header_format)
-    SystemSheet.merge_range('E1:F1', u'鏁版嵁搴撹繍琛岀洃鎺�', header_format)
-    SystemSheet.merge_range('G1:I1', u'绋嬪簭杩愯鐩戞帶', header_format)
-    SystemSheet.merge_range('J1:J2', u'鏁版嵁璐熻矗鍗曚綅', header_format)
-    SystemSheet.write('C2',u'缃戠粶鎯呭喌',header_format )
-    SystemSheet.write('D2',u'绯荤粺杩愯' ,header_format )
-    SystemSheet.write('E2',u'杩愯鐘舵��' ,header_format )
-    SystemSheet.write('F2',u'鍓嶄竴澶╂暟鎹�' ,header_format )
-    SystemSheet.write('G2',u'鏄惁鍚姩' ,header_format )
-    SystemSheet.write('H2',u'鏁版嵁搴撴煡璇�' ,header_format )
-    SystemSheet.write('I2',u'鎺㈤拡鏂囦欢' ,header_format )
+    SystemSheet.merge_range('A1:A2', u'省份', header_format)
+    SystemSheet.merge_range('B1:B2', u'服务器IP', header_format)
+    SystemSheet.merge_range('C1:D1', u'服务器状态监控', header_format)
+    SystemSheet.merge_range('E1:F1', u'数据库运行监控', header_format)
+    SystemSheet.merge_range('G1:I1', u'程序运行监控', header_format)
+    SystemSheet.merge_range('J1:J2', u'数据负责单位', header_format)
+    SystemSheet.write('C2',u'网络情况',header_format )
+    SystemSheet.write('D2',u'系统运行' ,header_format )
+    SystemSheet.write('E2',u'运行状态' ,header_format )
+    SystemSheet.write('F2',u'前一天数据' ,header_format )
+    SystemSheet.write('G2',u'是否启动' ,header_format )
+    SystemSheet.write('H2',u'数据库查询' ,header_format )
+    SystemSheet.write('I2',u'探针文件' ,header_format )
       
-#璁剧疆涓�鑷存�ф瘮瀵圭粨鏋滅殑鏍囬
+#设置一致性比对结果的标题
     IntactSheet.set_column('A:A',10)
-    IntactSheet.set_column('B:H',14)
-    IntactSheet.merge_range('A1:A2', u'鐪佷唤', header_format)
-    IntactSheet.merge_range('B1:B2', u'杩愯惀鍟�', header_format)
-    IntactSheet.merge_range('C1:F1', u'鎷ㄥ彿鏃ュ織璐ㄩ噺鐩戞帶', header_format)
-    IntactSheet.merge_range('G1:H1', u'涓�鑷存�ф瘮瀵圭粨鏋�', header_format)
-    IntactSheet.write('C2',u'鏃ュ織鐢熸垚',header_format )
-    IntactSheet.write('D2',u'鏃ュ織鏉℃暟',header_format )
-    IntactSheet.write('E2',u'甯愬彿鎬绘暟',header_format )
-    IntactSheet.write('F2',u'鍦ㄧ嚎甯愬彿鏁�',header_format )
-    IntactSheet.write('G2',u'姣斿鏃ュ織鏁�',header_format )
-    IntactSheet.write('H2',u'鏁版嵁瀹屾暣鐜�',header_format )
+    IntactSheet.set_column('B:I',14)
+    IntactSheet.merge_range('A1:A2', u'省份', header_format)
+    IntactSheet.merge_range('B1:B2', u'运营商', header_format)
+    IntactSheet.merge_range('C1:F1', u'拨号日志质量监控', header_format)
+    IntactSheet.merge_range('G1:H1', u'一致性比对结果', header_format)
+    IntactSheet.merge_range('I1:I2', u'数据负责单位', header_format)
+    IntactSheet.write('C2',u'日志生成',header_format )
+    IntactSheet.write('D2',u'日志条数',header_format )
+    IntactSheet.write('E2',u'帐号总数',header_format )
+    IntactSheet.write('F2',u'在线帐号数',header_format )
+    IntactSheet.write('G2',u'比对日志数',header_format )
+    IntactSheet.write('H2',u'数据完整率',header_format )
   
 workbook = xlw.Workbook(ReportPath)
-SystemSheet = workbook.add_worksheet(u"绯荤粺杩愯鐘舵��")
-IntactSheet = workbook.add_worksheet(u"涓�鑷存�ф瘮瀵圭粨鏋�") 
+SystemSheet = workbook.add_worksheet(u"系统运行状态")
+IntactSheet = workbook.add_worksheet(u"一致性比对结果") 
  
 Create_Header(SystemSheet, IntactSheet)
-Normal_format = workbook.add_format({'border':1})
-Warn_format = workbook.add_format({'fg_color':'red','border':1})
-NULL_format = workbook.add_format({'fg_color':'gray','border':1})
+Normal_format = workbook.add_format({'border':1,'align':'center','valign':'vcenter'})
+Warn_format = workbook.add_format({'fg_color':'red','border':1,'align':'center','valign':'vcenter'})
+NULL_format = workbook.add_format({'fg_color':'gray','border':1,'align':'center','valign':'vcenter'})
   
 fconf = open("province_name.ini", 'rb')
 IntactRow = 3
@@ -140,8 +142,8 @@ for Prov in fconf :
     CnName = unicode(CnName,"utf-8")
     Company = unicode(Company,"utf-8")
     if SystemStatus.has_key(AbbName) :
-        NetStatus = "yes"
-        SysStatus = "yes"
+        NetStatus = "YES"
+        SysStatus = "YES"
         ip = SystemStatus[AbbName]['ip']
         PStatus = SystemStatus[AbbName]['PStatus']
         DBstatus = SystemStatus[AbbName]['DBstatus']
@@ -153,41 +155,142 @@ for Prov in fconf :
         SystemSheet.write('C'+str(SystemRow),NetStatus,format)
         SystemSheet.write('D'+str(SystemRow),NetStatus,format)
 
-        if DBstatus == "no" :
+        if DBstatus == "NO" :
             format = Warn_format
         else :
             format = Normal_format
         SystemSheet.write('E'+str(SystemRow),DBstatus,format)
-        if DBDataStatus == "no" :
+        if DBDataStatus == "NO" :
             format = Warn_format
         else :
             format = Normal_format
         SystemSheet.write('F'+str(SystemRow),DBDataStatus,format)
-        if PStatus == "no" :
+        if PStatus == "NO" :
             format = Warn_format
         else :
             format = Normal_format
         SystemSheet.write('G'+str(SystemRow),PStatus,format)       
-        if DBFileStatus == "no":
+        if DBFileStatus == "NO":
             format = Warn_format
         else :
             format = Normal_format
         SystemSheet.write('H'+str(SystemRow),DBFileStatus,format)
-        if XMLStatus == "no":
+        if XMLStatus == "NO":
             format = Warn_format
         else :
             format = Normal_format
         SystemSheet.write('I'+str(SystemRow),XMLStatus,format)
         SystemSheet.write('J'+str(SystemRow),Company,Normal_format)
         SystemRow += 1
+	format = Normal_format
+# 完整性比对结果
+    IntactSheet.merge_range('A'+str(IntactRow)+':A'+str(IntactRow+1),CnName,Normal_format )
+    IntactSheet.write('B'+str(IntactRow),u'联通',Normal_format )
+    IntactSheet.write('B'+str(IntactRow + 1),u'电信',Normal_format )
+    IntactSheet.merge_range('I'+str(IntactRow)+':I'+str(IntactRow+1),Company,Normal_format )
+    if IntactStatus.has_key(AbbName):
         
-#     if IntactStatus.has_key(AbbName):
-#        if IntactStatus[AbbName].has_key('LT') :
+        if IntactStatus[AbbName].has_key('LT') :
+            LogNum = IntactStatus[AbbName]['LT']['LogNum']
+            Intact = IntactStatus[AbbName]['LT']['Intact']
+            if LogNum == 0 :
+                format = Warn_format
+            else :
+                format = Normal_format
+            IntactSheet.write('G'+str(IntactRow),LogNum, format)
+            if Intact == "0%" :
+                format = Warn_format
+            else :
+                format = Normal_format
+            IntactSheet.write('H' + str(IntactRow), Intact,format)
+        else :
+            IntactSheet.write('G' + str(IntactRow), "", NULL_format)
+            IntactSheet.write('H' + str(IntactRow), "", NULL_format)
+        if IntactStatus[AbbName].has_key('DX') :
+            LogNum = IntactStatus[AbbName]['DX']['LogNum']
+            Intact = IntactStatus[AbbName]['DX']['Intact']
+            if LogNum == 0 :
+                format = Warn_format
+            else :
+                format = Normal_format
+            IntactSheet.write('G'+str(IntactRow+1),LogNum, format)
+            if Intact == "0%" :
+                format = Warn_format
+            else :
+                format = Normal_format
+            IntactSheet.write('H' + str(IntactRow+1),Intact, format)
+        else :
+            IntactSheet.write('G' + str(IntactRow+1), "", NULL_format)
+            IntactSheet.write('H' + str(IntactRow+1), "", NULL_format)            
            
-        
-  
+    else :
+        IntactSheet.write('G' + str(IntactRow), "", NULL_format)
+        IntactSheet.write('H' + str(IntactRow), "", NULL_format)
+        IntactSheet.write('G' + str(IntactRow+1), "", NULL_format)
+        IntactSheet.write('H' + str(IntactRow+1), "", NULL_format)
+    format = Normal_format
+    if LogStatus.has_key(AbbName):
+
+        if LogStatus[AbbName].has_key('LT'):
+            LogUp = LogStatus[AbbName]['LT']['LogUp'] 
+            AccNum = LogStatus[AbbName]['LT']['AccNum']
+            liveNum = LogStatus[AbbName]['LT']['liveNum']
+            Src = LogStatus[AbbName]['LT']['Src']
+            DialIp = LogStatus[AbbName]['LT']['DialIp']
+            IntactSheet.write('C' + str(IntactRow), Src, format)
+            if LogUp == "YES" :
+                format = Normal_format
+            else :
+                format = Warn_format
+            IntactSheet.write('D' + str(IntactRow), LogUp, format)
+            IntactSheet.write('E' + str(IntactRow), AccNum, Normal_format)
+            if liveNum == '0' :
+                format = Warn_format
+            else :
+                format = Normal_format
+            IntactSheet.write('F' + str(IntactRow), liveNum, format)
+
+        else :
+            IntactSheet.write('C' + str(IntactRow), "", NULL_format)
+            IntactSheet.write('D' + str(IntactRow), "", NULL_format)
+            IntactSheet.write('E' + str(IntactRow), "", NULL_format)
+            IntactSheet.write('F' + str(IntactRow), "", NULL_format)
+            
+        if LogStatus[AbbName].has_key('DX'):
+            LogUp = LogStatus[AbbName]['DX']['LogUp'] 
+            AccNum = LogStatus[AbbName]['DX']['AccNum']
+            liveNum = LogStatus[AbbName]['DX']['liveNum']
+            Src = LogStatus[AbbName]['DX']['Src']
+            DialIp = LogStatus[AbbName]['DX']['DialIp']
+            IntactSheet.write('C' + str(IntactRow+1), Src, format)
+            if LogUp == "YES" :
+                format = Normal_format
+            else :
+                format = Warn_format
+            IntactSheet.write('D' + str(IntactRow+1), LogUp, format)
+            IntactSheet.write('E' + str(IntactRow+1), AccNum, Normal_format)
+            if liveNum == '0' :
+                format = Warn_format
+            else :
+                format = Normal_format
+            IntactSheet.write('F' + str(IntactRow+1), liveNum, format)
+
+        else :
+            IntactSheet.write('C' + str(IntactRow+1), "", NULL_format)
+            IntactSheet.write('D' + str(IntactRow+1), "", NULL_format)
+            IntactSheet.write('E' + str(IntactRow+1), "", NULL_format)
+            IntactSheet.write('F' + str(IntactRow+1), "", NULL_format)
+    else :
+            IntactSheet.write('C' + str(IntactRow), "", NULL_format)
+            IntactSheet.write('D' + str(IntactRow), "", NULL_format)
+            IntactSheet.write('E' + str(IntactRow), "", NULL_format)
+            IntactSheet.write('F' + str(IntactRow), "", NULL_format)        
+            IntactSheet.write('C' + str(IntactRow+1), "", NULL_format)
+            IntactSheet.write('D' + str(IntactRow+1), "", NULL_format)
+            IntactSheet.write('E' + str(IntactRow+1), "", NULL_format)
+            IntactSheet.write('F' + str(IntactRow+1), "", NULL_format)
+    IntactRow += 2
+    format = Normal_format
 workbook.close()
 
     
-
-
--- Excel_Report_new.py
+++ Excel_Report_new.py
@@ -21,7 +21,9 @@ def Config_init():
                 continue
             try :
                 CnName, AbbName, Ip, Company = pro_line.strip().split()
-            except ValueError as ve:
+                CnName = unicode(CnName,"utf-8")
+                Company = unicode(Company,"utf-8")
+            except ValueError as ve: 
                 print "Error line: ",pro_line
                 continue
             if not report_dic.has_key(AbbName):
@@ -29,8 +31,36 @@ def Config_init():
             else :
                 print AbbName,"repeat!!"
                 continue
-            
-
+            report_dic[AbbName]['system'] = {}
+            report_dic[AbbName]['logstat'] = {}
+            report_dic[AbbName]['intact'] = {}
+            report_dic[AbbName]['company'] = Company
+            report_dic[AbbName]['bc_ip'] = Ip
+            report_dic[AbbName]['CnName'] = CnName
+            report_dic[AbbName]['system']['net'] = ['NO',1]
+            report_dic[AbbName]['system']['pro'] = ['null',2]
+            report_dic[AbbName]['system']['dbstat'] = ['null',2]
+            report_dic[AbbName]['system']['dbdata'] = ['null',2]
+            report_dic[AbbName]['system']['dbfile'] = ['null',2]
+            report_dic[AbbName]['system']['xml'] = ['null',2]
+            report_dic[AbbName]['logstat']['dx'] = {}
+            report_dic[AbbName]['logstat']['lt'] = {}
+            report_dic[AbbName]['logstat']['dx']['ppp_ip'] = ['null',2]
+            report_dic[AbbName]['logstat']['dx']['upload'] = ['null',2]
+            report_dic[AbbName]['logstat']['dx']['acc_sum'] = ['null',2]
+            report_dic[AbbName]['logstat']['dx']['ali_sum'] = ['null',2]
+            report_dic[AbbName]['logstat']['dx']['src'] = ['null',2]
+            report_dic[AbbName]['logstat']['lt']['ppp_ip'] = ['null',2]
+            report_dic[AbbName]['logstat']['lt']['upload'] = ['null',2]
+            report_dic[AbbName]['logstat']['lt']['acc_sum'] = ['null',2]
+            report_dic[AbbName]['logstat']['lt']['ali_sum'] = ['null',2]
+            report_dic[AbbName]['logstat']['lt']['src'] = ['null',2]
+            report_dic[AbbName]['intact']['lt'] = {}
+            report_dic[AbbName]['intact']['dx'] = {}
+            report_dic[AbbName]['intact']['lt']['record'] = ['null',2]
+            report_dic[AbbName]['intact']['lt']['intact'] = ['null',2]
+            report_dic[AbbName]['intact']['dx']['record'] = ['null',2]
+            report_dic[AbbName]['intact']['dx']['intact'] = ['null',2]
         fconfig.close()
     except IOError as ioe :
         print ioe
@@ -38,21 +68,239 @@ def Config_init():
         
 
 def Load_log_system():
-    pass
+    SystemLog = TimePre + "-SystemStatus.txt"
+    SystemLogPath = os.path.join(DataDir, SystemLog)
+    try :
+        fsystem = open(SystemLogPath,'rb')
+        for record in fsystem :
+            if record.startswith('#') :
+                continue
+            recordList = record.strip().split()
+            if len(recordList) != 7 :
+                print "Colunm is fasle : ",record
+                continue
+            AbbName, Ip, Ps, DBs, DBd, xml, DBf =  recordList
+            report_dic[AbbName]['system']['net'] = ['YES',0]
+            if Ps.upper() == 'NO' :
+                report_dic[AbbName]['system']['pro'] = ['NO',1]
+            elif Ps.upper() == 'YES' :
+                report_dic[AbbName]['system']['pro'] = ['YES',0]
+                
+            if DBs.upper() == 'NO' :
+                report_dic[AbbName]['system']['dbstat'] = ['NO',1]
+            elif DBs.upper() == 'YES' :
+                report_dic[AbbName]['system']['dbstat'] = ['YES',0]
+                            
+            if DBd.upper() == 'NO' :
+                report_dic[AbbName]['system']['dbdata'] = ['NO',1]
+            elif DBd.upper() == 'YES' :
+                report_dic[AbbName]['system']['dbdata'] = ['YES',0]
+                
+            if xml.upper() == 'NO' :
+                report_dic[AbbName]['system']['xml'] = ['NO',1]
+            elif xml.upper() == 'YES' :
+                report_dic[AbbName]['system']['xml'] = ['YES',0]                
+
+            if DBf.upper() == 'NO' :
+                report_dic[AbbName]['system']['dbfile'] = ['NO',1]
+            elif DBf.upper() == 'YES' :
+                report_dic[AbbName]['system']['dbfile'] = ['YES',0]                            
+        fsystem.close()
+    except IOError as ioe :
+        print ioe
 
 def Load_log_logstat():
-    pass
+    BClog = TimePre + "-BClog.txt"
+    BClogPath = os.path.join(DataDir, BClog)
+    try :
+        fbclog = open(BClogPath,'rb')
+        for record in fbclog :
+            if record.startswith('#') :
+                continue
+            recordList = record.strip().split()
+            if len(recordList) != 7 :
+                print "Colunm is fasle : ",record
+                continue
+            AbbName, Isp, Ups, Accn, Alin, src, ppp_ip =  recordList
+            Isp = Isp.lower()
+            if Ups.upper() == 'NO' :
+                report_dic[AbbName]['logstat'][Isp]['upload'] = ['NO',1]
+            elif Ups.upper() == 'YES' :
+                report_dic[AbbName]['logstat'][Isp]['upload'] = ['YES',0]
+                
+            if Accn == '0' :
+                report_dic[AbbName]['logstat'][Isp]['acc_sum'] = [Accn,1]
+            else :
+                report_dic[AbbName]['logstat'][Isp]['acc_sum'] = [Accn,0]
+                            
+            if Alin == '0' :
+                report_dic[AbbName]['logstat'][Isp]['ali_sum'] = [Alin,1]
+            else :
+                report_dic[AbbName]['logstat'][Isp]['ali_sum'] = [Alin,0]
 
+            report_dic[AbbName]['logstat'][Isp]['src'] = [src,0]
+            report_dic[AbbName]['logstat'][Isp]['ppp_ip'] = [ppp_ip,0]
+                
+        fbclog.close()
+    except IOError as ioe :
+        print ioe
+    
 def Load_log_intact():
-    pass
+    IntactLog = TimePre + "-Intact.txt"
+    IntactLogPath = os.path.join(DataDir, IntactLog)
+    try :
+        finlog = open(IntactLogPath,'rb')
+        for record in finlog :
+            if record.startswith('#') :
+                continue
+            recordList = record.strip().split()
+            if len(recordList) != 4 :
+                print "Colunm is fasle : ",record
+                continue
+            AbbName, Isp, rec_sum, intact =  recordList
+            Isp = Isp.lower()
+            if rec_sum.upper() == '0' :
+                report_dic[AbbName]['intact'][Isp]['record'] = [rec_sum,1]
+            else :
+                report_dic[AbbName]['intact'][Isp]['record'] = [rec_sum,0]
+                
+            if intact == '0%' :
+                report_dic[AbbName]['intact'][Isp]['intact'] = [intact,1]
+            else :
+                report_dic[AbbName]['intact'][Isp]['intact'] = [intact,0]
+                            
+                
+        finlog.close()
+    except IOError as ioe :
+        print ioe
 
-
-def Create_excel():
-    pass
-
-def Write_excel():
-    pass
-
+def Create_excel(workbook,SystemSheet,IntactSheet):
+    header_format = workbook.add_format({
+                                         'bold':1,
+                                         'border':1,
+                                         'align':'center',
+                                         'valign':'vcenter',
+                                         'fg_color':'8DB4E3'
+                                         })
+    SystemSheet.set_column('A:C',14)
+    SystemSheet.set_column('D:J',14)
+    SystemSheet.merge_range('A1:A2', u'省份', header_format)
+    SystemSheet.merge_range('B1:B2', u'服务器IP', header_format)
+    SystemSheet.merge_range('C1:C2', u'网络状态', header_format)
+    SystemSheet.merge_range('D1:E1', u'数据库运行状态', header_format)
+    SystemSheet.merge_range('F1:H1', u'程序运行监控', header_format)
+    SystemSheet.merge_range('I1:I2', u'所属公司', header_format)
+    SystemSheet.write('D2',u'运行状态' ,header_format )
+    SystemSheet.write('E2',u'前一天数据' ,header_format )
+    SystemSheet.write('F2',u'是否启动' ,header_format )
+    SystemSheet.write('G2',u'数据库查询' ,header_format )
+    SystemSheet.write('H2',u'探针文件' ,header_format )
+      
+    IntactSheet.set_column('A:B',10)
+    IntactSheet.set_column('C:H',14)
+    IntactSheet.merge_range('A1:A2', u'省份', header_format)
+    IntactSheet.merge_range('B1:B2', u'运营商', header_format)
+    IntactSheet.merge_range('C1:F1', u'拨号日志质量监控', header_format)
+    IntactSheet.merge_range('G1:H1', u'一致性比对结果', header_format)
+    IntactSheet.merge_range('I1:I2', u'所属公司', header_format)
+    IntactSheet.write('C2',u'拨号日志生成',header_format )
+    IntactSheet.write('D2',u'日志条数',header_format )
+    IntactSheet.write('E2',u'账号总数',header_format )
+    IntactSheet.write('F2',u'在线账号数',header_format )
+    IntactSheet.write('G2',u'比对日志总数',header_format )
+    IntactSheet.write('H2',u'数据完整率',header_format )
+ 
+def Write_excel(workbook,sheet,site,value,state,mode):
+    """
+    sheet:表  ; site:表中位置 ; value:值 ; state:值对应的系统状态0正常，1告警，2为空  ;
+    mode:single代表在一个单元格中，merge代表合并单元格
+    """
+    Normal_format = workbook.add_format({'border':1,'align':'center','valign':'vcenter'})
+    Warn_format = workbook.add_format({'fg_color':'red','border':1,'align':'center','valign':'vcenter'})
+    NULL_format = workbook.add_format({'fg_color':'gray','border':1,'align':'center','valign':'vcenter'})
+    format = Normal_format
+    if state == 1 :
+        format = Warn_format
+    elif state == 2 :
+        format = NULL_format
+    
+    if mode == 'single':
+        sheet.write(site,value,format)
+    elif mode == "merge" :
+        sheet.merge_range(site,value,format)
+    
 def Report():
-    pass
+    workbook = xlw.Workbook(ReportPath)
+    SystemSheet = workbook.add_worksheet(u"系统运行")
+    IntactSheet = workbook.add_worksheet(u"一致性比对")
+    Create_excel(workbook,SystemSheet,IntactSheet)
+    
+    sys_index = 3
+    intact_index = 3
+    for AbbName, pro_dic in report_dic.iteritems() :
+        Ip = pro_dic['bc_ip']
+        Company = pro_dic['company']
+        CnName = pro_dic['CnName']
+        Write_excel(workbook,SystemSheet,'A' + str(sys_index),CnName, 0,'single')
+        Write_excel(workbook,SystemSheet,'B' + str(sys_index),Ip, 0,'single')
+        Write_excel(workbook,SystemSheet,'I' + str(sys_index),Company,0,'single')
+        Write_excel(workbook,IntactSheet,'A' + str(intact_index) +':A' +str(intact_index + 1 ),CnName, 0,'merge')
+        Write_excel(workbook,IntactSheet,'I' + str(intact_index) +':I' +str(intact_index + 1 ),Company, 0,'merge')
+        Write_excel(workbook,IntactSheet,'B' + str(intact_index),u'联通', 0,'single')
+        Write_excel(workbook,IntactSheet,'B' + str(intact_index + 1),u'电信', 0,'single')
 
+        net_v, net_s = pro_dic['system']['net']
+        pro_v, pro_s = pro_dic['system']['pro']
+        dbs_v, dbs_s = pro_dic['system']['dbstat']
+        dbd_v, dbd_s = pro_dic['system']['dbdata']
+        dbf_v, dbf_s = pro_dic['system']['dbfile']
+        xml_v, xml_s = pro_dic['system']['xml'] 
+        Write_excel(workbook,SystemSheet,'C' + str(sys_index),net_v, net_s,'single')
+        Write_excel(workbook,SystemSheet,'D' + str(sys_index),pro_v, pro_s,'single')
+        Write_excel(workbook,SystemSheet,'E' + str(sys_index),dbs_v, dbs_s,'single')
+        Write_excel(workbook,SystemSheet,'F' + str(sys_index),dbd_v, dbd_s,'single')
+        Write_excel(workbook,SystemSheet,'G' + str(sys_index),dbf_v, dbf_s,'single')
+        Write_excel(workbook,SystemSheet,'H' + str(sys_index),xml_v, xml_s,'single')
+		
+        lt_ip_v, lt_ip_s  = pro_dic['logstat']['lt']['ppp_ip'] 
+        lt_up_v, lt_up_s  = pro_dic['logstat']['lt']['upload'] 
+        lt_accn_v, lt_accn_s = pro_dic['logstat']['lt']['acc_sum']
+        lt_alin_v, lt_alin_s = pro_dic['logstat']['lt']['ali_sum']
+        lt_src_v, lt_src_s = pro_dic['logstat']['lt']['src']
+        lt_rec_v, lt_rec_s = pro_dic['intact']['lt']['record']
+        lt_int_v, lt_int_s = pro_dic['intact']['lt']['intact']
+
+        dx_ip_v, dx_ip_s  = pro_dic['logstat']['dx']['ppp_ip'] 
+        dx_up_v, dx_up_s  = pro_dic['logstat']['dx']['upload'] 
+        dx_accn_v, dx_accn_s = pro_dic['logstat']['dx']['acc_sum']
+        dx_alin_v, dx_alin_s = pro_dic['logstat']['dx']['ali_sum']
+        dx_src_v, dx_src_s = pro_dic['logstat']['dx']['src']
+        dx_rec_v, dx_rec_s = pro_dic['intact']['dx']['record']
+        dx_int_v, dx_int_s = pro_dic['intact']['dx']['intact']
+		
+        Write_excel(workbook,IntactSheet,'C' + str(intact_index),lt_up_v, lt_up_s,'single')
+        Write_excel(workbook,IntactSheet,'D' + str(intact_index),lt_rec_v, lt_rec_s,'single')
+        Write_excel(workbook,IntactSheet,'E' + str(intact_index),lt_accn_v, lt_accn_s,'single')
+        Write_excel(workbook,IntactSheet,'F' + str(intact_index),lt_alin_v, lt_alin_s,'single')
+        Write_excel(workbook,IntactSheet,'G' + str(intact_index),lt_rec_v, lt_rec_s,'single')
+        Write_excel(workbook,IntactSheet,'H' + str(intact_index),lt_int_v, lt_int_s,'single')
+
+        Write_excel(workbook,IntactSheet,'C' + str(intact_index+1),dx_up_v, dx_up_s,'single')
+        Write_excel(workbook,IntactSheet,'D' + str(intact_index+1),dx_rec_v, dx_rec_s,'single')
+        Write_excel(workbook,IntactSheet,'E' + str(intact_index+1),dx_accn_v, dx_accn_s,'single')
+        Write_excel(workbook,IntactSheet,'F' + str(intact_index+1),dx_alin_v, dx_alin_s,'single')
+        Write_excel(workbook,IntactSheet,'G' + str(intact_index+1),dx_rec_v, dx_rec_s,'single')
+        Write_excel(workbook,IntactSheet,'H' + str(intact_index+1),dx_int_v, dx_int_s,'single')
+
+        sys_index += 1
+        intact_index += 2
+    workbook.close()                                             
+    
+    
+if __name__ == "__main__" :
+    Config_init()
+    Load_log_system()
+    Load_log_logstat()
+    Load_log_intact()
+    print report_dic
+    Report()
